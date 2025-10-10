78b6a666a44592a059e415ab7a4738f39b1991fb
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-10 14:52:23 +0530

    handling iso format

--- backend/features.py
+++ backend/features.py
@@ -4,3 +4,9 @@ import pandas as pd
 from collections import Counter
 from datetime import datetime, timezone
 
+def parse_iso(ts):
+    try:
+        return pd.to_datetime(ts, utc = True)
+    except Exception:
+        return pd.to_datetime(ts)
+
