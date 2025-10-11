927d83b9df44ee1e415eeebd6eb8feab17dd7b98
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-11 13:49:15 +0530

    importing modules required

---
+++ backend/train_model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import joblib
+import os
+
