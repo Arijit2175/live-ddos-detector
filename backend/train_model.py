617bb37d5071bdb97a4dc1d8f063cd86a5ecb923
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-11 13:53:57 +0530

    logic for traning the model using random forest

--- backend/train_model.py
+++ backend/train_model.py
@@ -15,3 +15,26 @@ def load_data():
     print("[*] Label distribution:\n", df["label"].value_counts())
     return df
 
+def train_model(df):
+    features = ["pkts", "bytes", "avg_pkt_size",
+                "unique_srcs", "unique_dsts",
+                "tcp_ratio", "udp_ratio", "entropy_src"]
+    X = df[features]
+    y = df["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42, stratify=y
+    )
+
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    print("\n[*] Evaluation:")
+    print(classification_report(y_test, preds))
+
+    os.makedirs("models", exist_ok=True)
+    joblib.dump(model, MODEL_FILE)
+    print(f"[*] Model saved to {MODEL_FILE}")
+    return model
+
