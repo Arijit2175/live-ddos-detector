bf03ecc189fcc42f323982a67ca86c39ed8eca39
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-28 10:27:06 +0530

    setting up three js scene

--- frontend/script.js
+++ frontend/script.js
@@ -3,4 +3,14 @@
   container.style.width = '100%';
   container.style.height = '100%';
 
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera();
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+  camera.position.z = 400;
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  container.appendChild(renderer.domElement);
+
   
