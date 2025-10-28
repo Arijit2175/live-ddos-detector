6de11d78233b878ba51dbb242e990d7093ee1770
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-10-28 10:19:14 +0530

    constants

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,7 @@
 from flask import Flask, Response, send_from_directory, jsonify
 import time, json, os
 
+app = Flask(__name__, static_folder="../web-dashboard", static_url_path="/")
+
+ALERTS_FILE = "data/alerts.jsonl"
+
